bcd9c3b347b945cbc26dae30cf799a61316f4d7e
bwxzgeretou <bwxzgeretou@gmail.com>
2024-11-23 17:03:45 +0900

    lint

--- src/jma/jma_urls.ts
+++ src/jma/jma_urls.ts
@@ -1,32 +1,32 @@
 /**
  * 最新時刻のURL yyyy-mm-ddThh:mm:ss+09:00 (10分刻み)
  */
-const latest_time_url = "https://www.jma.go.jp/bosai/amedas/data/latest_time.txt"
+const latestTimeUrl = 'https://www.jma.go.jp/bosai/amedas/data/latest_time.txt'
 
 /**
- * 最新アメダスデータのURL
- * @param code アメダスコード
- * @param yyyymmdd yyyymmdd
- * @param hh 00, 03, 06, 09, 12, 15, 18, 21 (3時間刻み)
+ * @param code - アメダスコード
+ * @param yyyymmdd - yyyymmdd
+ * @param hh - 00, 03, 06, 09, 12, 15, 18, 21 (3時間刻み)
+ * @returns 最新アメダスデータのURL
  */
-function get_latest_amedas_url(code: string, yyyymmdd: string, hh: string): string {
-    // assert(hh === '00' || hh === '03' || hh === '06' || hh === '09' || hh === '12' || hh === '15' || hh === '18' || hh === '21')
-    return `https://www.jma.go.jp/bosai/amedas/data/point/${code}/${yyyymmdd}_${hh}.json`
+function getLatestAmedasUrl (code: string, yyyymmdd: string, hh: string): string {
+  // assert(hh === '00' || hh === '03' || hh === '06' || hh === '09' || hh === '12' || hh === '15' || hh === '18' || hh === '21')
+  return `https://www.jma.go.jp/bosai/amedas/data/point/${code}/${yyyymmdd}_${hh}.json`
 }
 
 /**
  * URL parameter みたいなものからコード (amdno) を取得する
- * e.g.: https://www.jma.go.jp/bosai/amedas/#area_type=offices&area_code=130000&amdno=44132&format=table10min&elems=53414
  * しかし、よく見ると ? ではなく # なので URL parameter ではない
+ * @param url - URL like https://www.jma.go.jp/bosai/amedas/#area_type=offices&area_code=130000&amdno=44132&format=table10min&elems=53414
+ * @returns amdno
  */
-function get_amdno_from_url(url: string): string {
-    const pattern = /[#&]amdno=(\d+)/
-    const matched = url.match(pattern)
-    if (!matched) {
-        throw new Error(`amdno not found in URL: ${url}`)
-    }
-    return matched[1]
+function getAmdnoFromUrl (url: string): string {
+  const pattern = /[#&]amdno=(\d+)/
+  const matched = url.match(pattern)
+  if (matched === null) {
+    throw new Error(`amdno not found in URL: ${url}`)
+  }
+  return matched[1]
 }
 
-
-export { latest_time_url, get_latest_amedas_url, get_amdno_from_url }
+export { latestTimeUrl, getLatestAmedasUrl, getAmdnoFromUrl }
--- src/jma/main.ts
+++ src/jma/main.ts
@@ -1,16 +1,16 @@
-"use strict";
+'use strict'
 // 1. 適切な最新のアメダスデータ https://www.jma.go.jp/bosai/amedas/data/point/{code}/{yyyymmdd}_{hh}.json を取得する
 // 2. 取得したデータから、絶対湿度 (enhance-abs-humidity), 露点温度 (enhance-dew-point) を算出する
 // 3. 算出したデータを、DOM操作によってテーブルに挿入する
-import { fetch_latest_amedas_data } from "./latest_amedas"
-import { get_amdno_from_url } from "./jma_urls"
+import { getAmdnoFromUrl } from './jma_urls'
+import { fetchLatestAmedasData } from './latest_amedas'
 
-const code = get_amdno_from_url(window.location.href)
+const code = getAmdnoFromUrl(window.location.href)
 
 // トップレベルのawaitを関数内に移動
-async function init() {
-    const data = await fetch_latest_amedas_data(code)
-    console.log(data)
+async function init (): Promise<void> {
+  const data = await fetchLatestAmedasData(code)
+  console.log(data)
 }
 
-init()
+await init()
