5e9700084b348bd58cdbc0ef01c664237bd3e037
bwxzgeretou <bwxzgeretou@gmail.com>
2024-11-23 23:50:12 +0900

    update main.ts to handle DOM change after loaded

--- src/jma/main.ts
+++ src/jma/main.ts
@@ -1,16 +1,42 @@
 'use strict'
-// 1. 適切な最新のアメダスデータ https://www.jma.go.jp/bosai/amedas/data/point/{code}/{yyyymmdd}_{hh}.json を取得する
+// 1. 最新のアメダスデータ https://www.jma.go.jp/bosai/amedas/data/point/{code}/{yyyymmdd}_{hh}.json を取得する
 // 2. 取得したデータから、絶対湿度 (enhance-abs-humidity), 露点温度 (enhance-dew-point) を算出する
 // 3. 算出したデータを、DOM操作によってテーブルに挿入する
+import {
+  getSeriestables,
+  type SeriestableRow,
+  appendColumnToSeriestable
+} from './dom_handler'
 import { getAmdnoFromUrl } from './jma_urls'
 import { fetchLatestAmedasData } from './latest_amedas'
 
 const code = getAmdnoFromUrl(window.location.href)
 
 // トップレベルのawaitを関数内に移動
-async function init (): Promise<void> {
-  const data = await fetchLatestAmedasData(code)
-  console.log(data)
-}
+// async function init (): Promise<void> {
+//   const data = await fetchLatestAmedasData(code)
+//   console.log(data)
+// }
 
-await init()
+// await init()
+
+// dom が更新された時に以下を実行する
+const observer = new MutationObserver(() =>
+  getSeriestables().forEach(seriestable => {
+    // stop observer
+    observer.disconnect()
+
+    const str: SeriestableRow = {
+      class: 'new-class',
+      headerValue: 'headerValue',
+      headerUnit: 'headerUnit',
+      values: ['value1', 'value2']
+    }
+    appendColumnToSeriestable(seriestable, str)
+
+    // start observer
+    observer.observe(seriestable, observeOptions)
+  })
+)
+const observeOptions = { attributes: true, childList: true, subtree: true }
+observer.observe(document.documentElement, observeOptions)
