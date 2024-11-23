0560d61a256816a84861d5325e56d8c5a53359e5
bwxzgeretou <bwxzgeretou@gmail.com>
2024-11-23 12:33:29 +0900

    initial coomit (reference: https://qiita.com/munieru_jp/items/ce57ec8c839f4a8727be)

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ignorePatterns: [
+      'dist/**/*.js'
+    ],
+    extends: [
+      '@munierujp/eslint-config-typescript',
+      'plugin:jest/recommended'
+    ],
+    parserOptions: {
+      project: './tsconfig.json'
+    }
+  }
+  
