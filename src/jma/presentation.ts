1fcd7925c124e120aa4687f154a79da921c928d4
bwxzgeretou <bwxzgeretou@gmail.com>
2024-11-25 21:00:06 +0900

    add presentation

---
+++ src/jma/presentation.ts
@@ -0,0 +1,33 @@
+import { type SeriestableRow } from './dom_handler'
+import { type AmedasData } from './jma_amedas_fetcher'
+import { HumidCalculator } from './math'
+
+const VOLUMETRIC_HUMIDITY_CLASS = 'td-volumetric-humidity'
+const DEW_POINT_CLASS = 'td-dew-point'
+
+const VALUES_PRECISION = 2
+
+// AmedasData の配列を SeriestableRow (容積湿度, 露点温度) に変換する
+export function convertAmedasDataToSeriestableRow (amedasDatas: AmedasData[]): [SeriestableRow, SeriestableRow] {
+  const volumetricHumidityValues: number[] = []
+  const dewPointValues: number[] = []
+  for (const amedasData of amedasDatas) {
+    const pressure = amedasData.pressure ?? 1013.25
+    const humidCalculator = new HumidCalculator(amedasData.temperature, amedasData.humidity, pressure)
+    volumetricHumidityValues.push(humidCalculator.volumetricHumidity)
+    dewPointValues.push(humidCalculator.dewPoint)
+  }
+  const volumetricHumidityRow: SeriestableRow = {
+    class: VOLUMETRIC_HUMIDITY_CLASS,
+    headerValue: '容積絶対湿度',
+    headerUnit: 'g/㎥',
+    values: volumetricHumidityValues.map(value => value.toFixed(VALUES_PRECISION))
+  }
+  const dewPointRow: SeriestableRow = {
+    class: DEW_POINT_CLASS,
+    headerValue: '露点温度',
+    headerUnit: '℃',
+    values: dewPointValues.map(value => value.toFixed(VALUES_PRECISION))
+  }
+  return [volumetricHumidityRow, dewPointRow]
+}
